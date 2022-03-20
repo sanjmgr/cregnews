0c03cb1171b3ae020e1d34cc3a3ff4b17c8c3008
sanjmgr <sanjmgr05@gmail.com>
2022-03-20 19:14:30 +0545

    access control allow origin

--- src/worker/Http.js
+++ src/worker/Http.js
@@ -4,7 +4,8 @@ const HTTP = axios.create({
     baseURL: `https://newsapi.org/v2/`,
     headers: {
         Authorization: 'Bearer 8cd9d8bbae394be5b04b953c45f57454',
-        'content-type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
     },
 })
 
