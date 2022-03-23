d59f287e1d518c8c31bd8ea04117a0e9e4a56f5a
sanjmgr <sanjmgr05@gmail.com>
2022-03-23 21:51:13 +0545

    added skeleton loader

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -6,7 +6,7 @@
         <div class="flex flex-col justify-between md:flex-row md:gap-16">
             <div class="divide-y rounded-md md:basis-1/2 lg:basis-2/5">
                 <HashTag :tags="tags_1" />
-                <div v-if="primary.length">
+                <template v-if="isLoaded">
                     <NewsCard
                         v-for="(article, index) in primary"
                         :key="article"
@@ -14,28 +14,43 @@
                         :article="article"
                         :type="shuffle_1[index]"
                     />
-                </div>
+                </template>
+                <template v-else>
+                    <SkeletonCard type="primary" />
+                </template>
             </div>
             <div class="md:basis-1/2 md:divide-y lg:basis-3/5">
                 <HashTag class="hidden md:flex" :tags="tags_2" />
                 <div class="flex flex-col md:gap-1 lg:flex-row lg:gap-16">
                     <div class="rounded-md lg:basis-1/2">
-                        <NewsCard
-                            v-for="(article, index) in secondary"
-                            :key="article"
-                            :index="index"
-                            :article="article"
-                            :type="shuffle_2[index]"
-                        />
+                        <template v-if="isLoaded">
+                            <NewsCard
+                                v-for="(article, index) in secondary"
+                                :key="article"
+                                :index="index"
+                                :article="article"
+                                :type="shuffle_2[index]"
+                            />
+                        </template>
+                        <template v-else>
+                            <SkeletonCard type="tertiary" />
+                            <SkeletonCard type="secondary" />
+                        </template>
                     </div>
                     <div class="rounded-md lg:block lg:basis-1/2">
-                        <NewsCard
-                            v-for="(article, index) in tertiary"
-                            :key="article"
-                            :index="index"
-                            :article="article"
-                            :type="shuffle_3[index]"
-                        />
+                        <template v-if="isLoaded">
+                            <NewsCard
+                                v-for="(article, index) in tertiary"
+                                :key="article"
+                                :index="index"
+                                :article="article"
+                                :type="shuffle_3[index]"
+                            />
+                        </template>
+                        <template v-else>
+                            <SkeletonCard type="secondary" />
+                            <SkeletonCard type="tertiary" />
+                        </template>
                     </div>
                 </div>
             </div>
@@ -47,13 +62,23 @@
 import HashTag from './HashTag.vue'
 import { articles } from '../assets/articles'
 import NewsCard from './NewsCard.vue'
+import SkeletonCard from './SkeletonCard.vue'
 import CommandPalette from './CommandPalette.vue'
+import { onMounted, ref, Suspense } from 'vue'
 
 import { chunkIntoN, shuffle_1, shuffle_2, shuffle_3, tags_1, tags_2 } from '../utility'
 
 export default {
     setup() {
         const { 0: primary, 1: secondary, 2: tertiary } = chunkIntoN(articles, 3)
+        const isLoaded = ref(false)
+
+        onMounted(() => {
+            setTimeout(() => {
+                isLoaded.value = true
+                console.log('Here time out...')
+            }, 5000)
+        })
 
         return {
             primary,
@@ -64,8 +89,9 @@ export default {
             shuffle_3,
             tags_1,
             tags_2,
+            isLoaded,
         }
     },
-    components: { HashTag, NewsCard, CommandPalette },
+    components: { Suspense, SkeletonCard, HashTag, NewsCard, CommandPalette },
 }
 </script>
---
+++ src/components/SkeletonCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="py-6">
+        <div class="flex items-center gap-2">
+            <div class="h-8 w-8 animate-pulse rounded-full bg-[#E6E9ED]"></div>
+            <div class="h-6 w-[25%] animate-pulse rounded-md bg-[#E6E9ED]"></div>
+            <div class="h-6 w-[20%] animate-pulse rounded-2xl bg-[#E6E9ED]"></div>
+        </div>
+        <article class="mt-4 mb-3 flex flex-col">
+            <div class="space-y-2">
+                <div class="h-6 w-[72%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+                <div class="h-6 w-[80%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+                <div class="h-6 w-[90%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+                <div class="h-6 w-[40%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+            </div>
+            <div
+                :class="[
+                    'aspect-w-3 aspect-h-2 my-4 animate-pulse rounded-2xl bg-[#E6E9ED]',
+                    type === 'tertiary' ? 'order-first md:hidden' : 'order-1',
+                ]"
+            ></div>
+            <div v-if="type === 'primary'" class="order-last space-y-2">
+                <div class="h-5 w-[85%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+                <div class="h-5 w-[90%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+                <div class="h-5 w-[92%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+                <div class="h-5 w-[86%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+                <div class="h-5 w-[40%] animate-pulse rounded-sm bg-[#E6E9ED]" />
+            </div>
+        </article>
+
+        <div class="flex items-center justify-between space-x-2 text-secondary">
+            <div class="flex w-1/2 items-center gap-2">
+                <div class="h-4 w-40 animate-pulse rounded-sm bg-[#E6E9ED]"></div>
+                <div class="h-1 w-1 animate-pulse rounded-full bg-secondary" />
+                <div class="h-4 w-40 animate-pulse rounded-sm bg-[#E6E9ED]"></div>
+            </div>
+            <div class="h-4 w-1/2 animate-pulse rounded-sm bg-[#E6E9ED]"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['type'],
+}
+</script>
